34460d780b2f75d35f307e2ebffa7218db9ab761
Saratonhaa339 <130287350+Saratonhaa339@users.noreply.github.com>
2023-06-15 15:46:37 +0300

    Add files via upload

---
+++ MAGAZ-2.0-main/goods.js
@@ -0,0 +1,170 @@
+const goods = [{
+		id: 1,
+		name: 'MSI GeForce GTX 1650 D6 VENTUS',
+		price: 15600,
+		imgSrc: 'img/card1.jpg',
+	}, {
+		id: 2,
+		name: 'Капибара',
+		price: 222,
+		imgSrc: 'goods/kapibara.png',
+	}, {
+		id: 3,
+		name: 'Процессор Phenom II X4',
+		price: 333,
+		imgSrc: 'img/amd.jpg',
+	}, {
+		id: 4,
+		name: 'Материнская плата GIGABYTE A320M-A PRO',
+		price: 444,
+		imgSrc: 'img/pl2.jpg',
+	}, {
+		id: 5,
+		name: 'Ноутбук Maibenben M543',
+		price: 555,
+		imgSrc: 'img/nout1.jpg',
+	}, {
+		id: 6,
+		name: 'Доктор Ливси',
+		price: 666,
+		imgSrc: 'goods/doctor livesey.png',
+	}, {
+		id: 7,
+		name: 'машинка',
+		price: 1000000000,
+		imgSrc: 'goods/car.png',
+	}, {
+		id: 8,
+		name: 'Шлёпа',
+		price: 1111,
+		imgSrc: 'goods/shlepa.png',
+	}, {
+		id: 9,
+		name: 'Электро Тёрка',
+		price: 3500,
+		imgSrc: 'img/terka.jpg',
+	}, {
+		id: 10,
+		name: 'Тяги',
+		price: 333,
+		imgSrc: 'goods/tyagi.png',
+	}, {
+		id: 11,
+		name: 'Блинный Кот',
+		price: 444,
+		imgSrc: 'goods/blin.png',
+	}, {
+		id: 12,
+		name: 'Смекалочка',
+		price: 555,
+		imgSrc: 'goods/smekalochka.png',
+	}, {
+		id: 13,
+		name: 'Окно (Пластиковое)',
+		price: 13000,
+		imgSrc: 'img/ocno.jpg',
+	},{
+		id: 14,
+		name: 'Hogwarts Legacy',
+		price: 12000,
+		imgSrc: 'img/hovads.jpg',
+	},
+	{
+		id: 101,
+		name: 'Шлёпа',
+		image: 'goods/shlepa.png',
+		description: 'Описание товара',
+		price: 100
+	},
+	{
+		id: 102,
+		name: 'Капибара',
+		image: 'goods/kapibara.png',
+		description: 'Описание товара',
+		price: 200
+	},
+	{
+		id: 103,
+		name: 'Тяги',
+		image: 'goods/tyagi.png',
+		description: 'Описание товара',
+		price: 300
+	},
+	{
+		id: 104,
+		name: 'Блинный Кот',
+		image: 'goods/blin.png',
+		description: 'Описание товара',
+		price: 100
+	},
+	{
+		id: 105,
+		name: 'Смекалочка',
+		image: 'goods/smekalochka.png',
+		description: 'Описание товара',
+		price: 200
+	},
+	{
+		id: 106,
+		name: 'Доктор Ливси',
+		image: 'goods/doctor livesey.png',
+		description: 'Описание товара',
+		price: 300
+	},
+	{
+		id: 107,
+		name: 'Машинка',
+		image: 'goods/car.png',
+		description: 'Описание товара',
+		price: 100
+	},
+	{
+		id: 108,
+		name: 'Шлёпа',
+		image: 'goods/shlepa.png',
+		description: 'Описание товара',
+		price: 100
+	},
+	{
+		id: 109,
+		name: 'Капибара',
+		image: 'goods/kapibara.png',
+		description: 'Описание товара',
+		price: 200
+	},
+	{
+		id: 110,
+		name: 'Тяги',
+		image: 'goods/tyagi.png',
+		description: 'Описание товара',
+		price: 300
+	},
+	{
+		id: 111,
+		name: 'Блинный Кот',
+		image: 'goods/blin.png',
+		description: 'Описание товара',
+		price: 100
+	},
+	{
+		id: 112,
+		name: 'Смекалочка',
+		image: 'goods/smekalochka.png',
+		description: 'Описание товара',
+		price: 200
+	},
+	{
+		id: 113,
+		name: 'Доктор Ливси',
+		image: 'goods/doctor livesey.png',
+		description: 'Описание товара',
+		price: 300
+	},
+	{
+		id: 114,
+		name: 'Машинка',
+		image: 'goods/car.png',
+		description: 'Описание товара',
+		price: 100
+	},
+];
